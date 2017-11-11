0a95c01f6ccbdba2511b8e12af8b977956daf98c
Vicente Salinas <vicente.salinas@ttu.edu>
2017-11-11 15:31:36 -0600

    Added default w value to 0.

--- calc.py
+++ calc.py
@@ -4,7 +4,9 @@ def wind_speed(u, v, w):
     '''
     Calculates wind speed from u, v and w componants.
     '''
-    return np.sqrt(u*u + v*v + w*w)
+    w = 0.
+    wind = np.sqrt(u*u + v*v + w*w)
+    return(wind)
 
 print(wind_speed(10,-23,2))
 
