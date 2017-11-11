cb3f793926bc742bc084cd141ee28ad05b34cdab
Vicente Salinas <vicente.salinas@ttu.edu>
2017-11-11 16:40:22 -0600

    Definitely fixed calc.py

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def wind_speed(u, v, w):
     Calculates wind speed from u, v and w componants.
     '''
     w = 0.
-    wind = np.sqrt(u*u + v*v + w*w)
+    wind = (u*u + v*v + w*w)
     return(wind)
 
 #print(wind_speed(10,-23,2))
