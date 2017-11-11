95926aff0d55ca36d71c53a3d225d7429318ff6f
Vicente Salinas <vicente.salinas@ttu.edu>
2017-11-11 15:28:40 -0600

    Added w-component to wind speed calculation.

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def wind_speed(u,v):
+def wind_speed(u, v, w):
     '''
-    Calculates wind speed from u and v componants.
+    Calculates wind speed from u, v and w componants.
     '''
-    return np.sqrt(u*u + v*v)
+    return np.sqrt(u*u + v*v + w*w)
 
-print(wind_speed(10,-23))
+print(wind_speed(10,-23,2))
 
